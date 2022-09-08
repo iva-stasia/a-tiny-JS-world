6026e545bb8eafc8ec30443af31347cb0f547735
Anastasiia Ivanchenko <iva21sta@gmail.com>
2022-09-08 16:21:31 +0300

    creation tiny js world

--- index.js
+++ index.js
@@ -7,24 +7,61 @@ import { print } from './js/lib.js';
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+   species: 'dog',
+   name: 'Ozzy',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'Woof-woof!',
+};
 
+const cat = {
+   species: 'cat',
+   name: 'Debbie',
+   gender: 'female',
+   legs: 4,
+   hands: 0,
+   saying: 'Meeeow!',
+};
+
+const women = {
+   species: 'human',
+   name: 'Gwen',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: 'Hi there!'
+};
+
+const man = {
+   species: 'human',
+   name: 'David',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'What\'s up?'
+};
+
+const catWomen = {
+   species: 'cat-human',
+   name: 'Dita',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: cat.saying
+};
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const inhabitants = [dog, cat, women, man, catWomen]
+const inhabitantsKeys = ['species', 'name', 'gender', 'legs', 'hands', 'saying']
+const inhabitantsInfo = []
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+inhabitants.map(inhabitant => {
+   inhabitantsInfo.push(
+      [inhabitantsKeys.map(key => inhabitant[key]).join('; ')]
+   )
+})
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+inhabitantsInfo.forEach(inhabitantInfo => print(inhabitantInfo))
